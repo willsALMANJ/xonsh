91e19fdf0d56fefafe712389edef5d601552ebeb
Anthony Scopatz <scopatz@gmail.com>
2015-03-28 17:18:12 -0500

    troff edits

--- xonsh/man/troff.py
+++ xonsh/man/troff.py
@@ -4,8 +4,10 @@ import re
 import sys
 
 from ply import lex
+from ply import yacc
 from ply.lex import TOKEN
 
+from xonsh.parser import Location
 
 class TroffLexer(object):
     """A lexer for troff."""
@@ -128,3 +130,100 @@ class TroffLexer(object):
     def t_error(self, t):
         msg = 'Invalid token {0!r}'.format(t.value[0])
         self._error(msg, t)
+
+
+
+class TroffParser(object):
+    """A class that parses the troff language."""
+
+    def __init__(self, lexer_optimize=True, lexer_table='xonsh.man.lexer_table',
+                 yacc_optimize=True, yacc_table='xonsh.man.parser_table',
+                 yacc_debug=False, outputdir=None):
+        """Parameters
+        ----------
+        lexer_optimize : bool, optional
+            Set to false when unstable and true when lexer is stable.
+        lexer_table : str, optional
+            Lexer module used when optimized.
+        yacc_optimize : bool, optional
+            Set to false when unstable and true when parser is stable.
+        yacc_table : str, optional
+            Parser module used when optimized.
+        yacc_debug : debug, optional
+            Dumps extra debug info.
+        outputdir : str or None, optional
+            The directory to place generated tables within.
+        """
+        self.lexer = lexer = TroffLexer()
+        self.tokens = lexer.tokens
+
+        yacc_kwargs = dict(module=self, debug=yacc_debug,
+                           start='start_symbols', optimize=yacc_optimize,
+                           tabmodule=yacc_table)
+        if not yacc_debug:
+            yacc_kwargs['errorlog'] = yacc.NullLogger()
+        if outputdir is not None:
+            yacc_kwargs['outputdir'] = outputdir
+        self.parser = yacc.yacc(**yacc_kwargs)
+
+        # Keeps track of the last token given to yacc (the lookahead token)
+        self._last_yielded_token = None
+
+    def reset(self):
+        """Resets for clean parsing."""
+        self.lexer.reset()
+        self._last_yielded_token = None
+
+    def parse(self, s, filename='<code>', debug_level=0):
+        """Returns an abstract syntax tree of troff code.
+
+        Parameters
+        ----------
+        s : str
+            The troff code.
+        filename : str, optional
+            Name of the file.
+        debug_level : str, optional
+            Debugging level passed down to yacc.
+
+        Returns
+        -------
+        tree : AST
+        """
+        self.reset()
+        self.lexer.fname = filename
+        tree = self.parser.parse(input=s, lexer=self.lexer,
+                                 debug=debug_level)
+        return tree
+
+    def _yacc_lookahead_token(self):
+        """Gets the last token seen by the lexer."""
+        return self.lexer.last
+
+    def currloc(self, lineno, column=None):
+        """Returns the current location."""
+        return Location(fname=self.lexer.fname, lineno=lineno,
+                        column=column)
+
+    def token_col(self, t):
+        """Gets ths token column"""
+        return t.lexpos
+
+    @property
+    def lineno(self):
+        if self.lexer.last is None:
+            return 0
+        else:
+            return self.lexer.last.lineno
+
+    @property
+    def col(self):
+        t = self._yacc_lookahead_token()
+        if t is not None:
+            return self.token_col(t)
+        return 1
+
+    def _parse_error(self, msg, loc):
+        err = SyntaxError('{0}: {1}'.format(loc, msg))
+        err.loc = loc
+        raise err
