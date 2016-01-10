43d57591a50b1b48a35a8ccf3f63ba4f295e107c
adam j hartz <hartz@mit.edu>
2016-01-09 21:31:19 -0500

    remove test case that will no longer succeed after parser change

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1451,9 +1451,6 @@ def test_dollar_py_test_recursive_test():
     yield check_xonsh_ast, {'WAKKA': 42, 'JAWAKA': 'WAKKA'}, \
                            '${${"JAWA" + $JAWAKA[-2:]}}'
 
-def test_dollar_name_set():
-    yield check_xonsh, {'WAKKA': 42}, '$WAKKA = 42'
-
 def test_dollar_py_set():
     yield check_xonsh, {'WAKKA': 42}, 'x = "WAKKA"; ${x} = 65'
 
